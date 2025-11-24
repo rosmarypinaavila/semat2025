264e5e00e3d8b536036a9989316e0bd43265d546
ronald460 <rgimenez877@gmail.com>
2025-11-24 11:05:09 -0400

    update 24-11-2025

--- act_econ/views.py
+++ act_econ/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def empresas(request):
+    return render(request, 'act_econ/empresas.html')
--- semat2025/urls.py
+++ semat2025/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
     path('', include('personal.urls')),
+    path('', include('act_econ.urls')),
 
 ]
